10334aa9cfeaab384e4efc17bee4d31313bc4e4a
imper <me@imper.info>
2019-09-23 19:53:39 +0200

    initial commit

---
+++ src/XMLReader.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright: IMPER.INFO Adrian Szuszkiewicz
+ * Date: 23.09.2019
+ * Time: 19:47
+ */
+
+namespace Imper86\XMLReader;
+
+
+class XMLReader extends \XMLReader
+{
+    public function getIterator(string $iterableTagName): \Iterator
+    {
+        while ($this->read() && $this->name !== $iterableTagName) {}
+
+        while (true) {
+            if ($this->name !== $iterableTagName) {
+                if ($this->next()) {
+                    continue;
+                }
+
+                break;
+            }
+
+            yield simplexml_load_string($this->readOuterXml());
+
+            if ($this->next()) {
+                continue;
+            }
+
+            break;
+        }
+    }
+}
